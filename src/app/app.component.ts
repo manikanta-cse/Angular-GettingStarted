54e934d86366b529c95e3c23917e100fcb6657be
Kattamuri, Manikanta <Manikanta.Kattamuri@cdk.com>
2017-03-30 17:28:08 +0530

    day1 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'pm-app',
-    template: `
-        <h1>Angular2: Getting Started</h1>
-    `
+
+    selector:'pm-app',
+    template:`
+    <div> <h1>{{pageTitle}}</h1>
+       <pm-products></pm-products>
+        </div>`
+
 })
-export class AppComponent { }
+
+export class AppComponent{
+    pageTitle: string = "Acme Product Mgmt"
+    
+}
+
+//the three key areas of Component : class, metadata(Decorator), import
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import {ProductListComponent} from './products/product-list.component';
+import { ProductFilterPipe } from './products/product-filter.pipe';
+
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [ BrowserModule,FormsModule ],
+  declarations: [ AppComponent,ProductListComponent,ProductFilterPipe ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
