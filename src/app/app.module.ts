676e3f6e4158ffb9366671c2d121cdcd0c1627bc
Kattamuri, Manikanta <Manikanta.Kattamuri@cdk.com>
2017-03-31 16:27:51 +0530

    implemented routing:day2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import {WelcomeComponent} from './home/welcome.component';
+
+import {ProductDetailComponent} from './products/product-detail.component';
 import {ProductListComponent} from './products/product-list.component';
+import {ProductDetailGuard} from './products/product-guard.service';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import {StarComponent} from './shared/star.component'
 
 
+
+
 @NgModule({
-  imports: [ BrowserModule,FormsModule ,HttpModule],
-  declarations: [ AppComponent,ProductListComponent,ProductFilterPipe,StarComponent ],
+  imports: [ BrowserModule,FormsModule ,HttpModule,RouterModule.forRoot([
+     { path:'products', component: ProductListComponent},
+     {path:'product/:id',canActivate:[ProductDetailGuard],component:ProductDetailComponent},
+     {path:'welcome',component:WelcomeComponent},
+     {path:'',redirectTo:'welcome',pathMatch:'full'},
+     {path:'**',redirectTo:'welcome',pathMatch:'full'}
+  ])],
+  providers:[ProductDetailGuard],
+   declarations: [ AppComponent,WelcomeComponent,ProductListComponent,ProductDetailComponent,ProductFilterPipe,StarComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
